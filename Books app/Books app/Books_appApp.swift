e7e2ca7fcb62fd4eaf42453797e9c36485a895f0
lalasuleymann <laslymn12@gmail.com>
2026-08-09 01:52:16 +0400

    repo clonned

---
+++ Books app/Books app/Books_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Books_appApp.swift
+//  Books app
+//
+//  Created by Lala Suleymanova on 09.08.26.
+//
+
+import SwiftUI
+
+@main
+struct Books_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
