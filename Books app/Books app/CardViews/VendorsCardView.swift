1ed0c7045546e8fc74e2bfa551f8beba8309abd8
lalasuleymann <laslymn12@gmail.com>
2026-08-10 21:08:12 +0400

    vendors page added

---
+++ Books app/Books app/CardViews/VendorsCardView.swift
@@ -0,0 +1,18 @@
+//
+//  VendorsCardView.swift
+//  Books app
+//
+//  Created by Lala Suleymanova on 10.08.26.
+//
+
+import SwiftUI
+
+struct VendorsCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VendorsCardView()
+}
